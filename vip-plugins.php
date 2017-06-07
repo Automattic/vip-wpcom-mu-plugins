732f19b8c282d6b861c9f2d0117da0ee3b14613e
David Binovec <david.binovec@gmail.com>
2017-06-07 09:22:08 +0000

    Remove the shortcode-reversals.php mu-plugin in favour of automatically enabling Shortcode module of Jetpack
    The Shortcode module contains the very same class and some other useful functionality, which should be present in development environment, as the code is being automatically loaded in production. Eg.: Instagram embed which is replacing Instagram oembed.

--- vip-plugins.php
+++ vip-plugins.php
@@ -5,3 +5,11 @@ require __DIR__ . '/jetpack/jetpack.php';
 require __DIR__ . '/media-explorer/media-explorer.php';
 require __DIR__ . '/Rewrite-Rules-Inspector/rewrite-rules-inspector.php';
 require __DIR__ . '/writing-helper/writing-helper.php';
+
+/*
+ * Automatically activate the shortcodes module as the same code
+ * is being automatically present on the WordPress.com VIP platform.
+ */
+if ( class_exists( 'Jetpack' ) ) {
+	Jetpack::activate_module( 'shortcodes', false, false );
+}
